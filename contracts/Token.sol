b73ed0451033c621e918e22ca9d0064933022ca3
Alberto Abuchaibe <aabuchaibe11@gmail.com>
2023-08-31 12:28:01 -0500

    7. Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -24,11 +24,18 @@ contract Token {
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-        balanceOf[_to] = balanceOf[_to] + _value;
-        emit Transfer(msg.sender, _to, _value);
+        
+        _transfer(msg.sender, _to, _value);
         return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0));
+        
+        balanceOf[_from] = balanceOf[_from] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(_from, _to, _value);
     }
 
     function approve(address _spender, uint256 _value) public returns(bool success) {
@@ -36,6 +43,17 @@ contract Token {
         allowance[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require (_value > 0 && balanceOf[_from] >= _value );
+        require (_value <= allowance[_from][msg.sender]);
+
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+        _transfer(_from, _to, _value);
+
         return true;
     }
 }
